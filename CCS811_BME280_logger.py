11193f55498b51b01ed0a60991171362d5fa8ea6
reps <marco@reps.cc>
2025-01-28 18:46:22 +0100

    Update CCS811_BME280_logger.py

--- CCS811_BME280_logger.py
+++ CCS811_BME280_logger.py
@@ -1,22 +1,59 @@
 import smbus2
 import bme280
+import configparser
+from influxdb_interface import influx_writer
+from datetime import datetime
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+from time import time
+
+
+class influx_writer:
+
+    def __init__(self, url, token, org):
+        self.client = InfluxDBClient(url=url, token=token, org=org)
+        self.write_api = self.client.write_api(write_options=SYNCHRONOUS)
+        
+    def write(self, bucket, measurement, field, val, timestamp=None, tags=None):
+        if not timestamp:
+            timestamp = datetime.now()
+        logging.debug('writing point to influxdb: measurement=%s field=%s val=%s'%(str(measurement),str(field),str(val)))
+        p = Point(measurement).field(field, float(val)).time(timestamp, WritePrecision.MS)
+        if tags is not None:
+            for t in tags:
+                p.tag(t[0], t[1])
+        logging.debug('point made')
+        try:
+            self.write_api.write(bucket, record=p)
+        except Exception as exc:
+            logging.error(exc)
+            pass
+        logging.debug('point written, writer done')
+        
+        
+        
+        
+
+config = configparser.ConfigParser()
+config.read('conf.ini')
+influx_url = config['INFLUX']['url']
+influx_token = config['INFLUX']['token']
+influx_org = config['INFLUX']['org']
+
+writer=influx_writer(influx_url, influx_token, influx_org)
 
 port = 1
-address = 0x77
+bme280_address = 0x77
 bus = smbus2.SMBus(port)
 
-calibration_params = bme280.load_calibration_params(bus, address)
+calibration_params = bme280.load_calibration_params(bus, bme280_address)
 
-# the sample method will take a single reading and return a
-# compensated_reading object
-data = bme280.sample(bus, address, calibration_params)
 
-# the compensated_reading class has the following attributes
-print(data.id)
-print(data.timestamp)
-print(data.temperature)
-print(data.pressure)
-print(data.humidity)
+while(True):
+    data = bme280.sample(bus, bme280_address, calibration_params)
+    writer.write('lab_sensors', 'Ambient_Temp', 'BME280', data.temperature)
+    writer.write('lab_sensors', 'Ambient_Pressure', 'BME280', data.pressure)
+    writer.write('lab_sensors', 'Ambient_Humidity', 'BME280', data.humidity)
+    time.sleep(1)
 
-# there is a handy string representation too
-print(data)
+
